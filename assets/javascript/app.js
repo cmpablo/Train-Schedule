fdd157d5e5c8dc924e639a658c04e2b8751d8701
Christine Pablo <cmpablo@cpablo.local>
2018-04-10 17:01:32 -0400

    added initial layout and styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyByZO9JCJ0lCSMT3LAST2RdIFZKfdRzka8",
+    authDomain: "train-schedule-79421.firebaseapp.com",
+    databaseURL: "https://train-schedule-79421.firebaseio.com",
+    projectId: "train-schedule-79421",
+    storageBucket: "train-schedule-79421.appspot.com",
+    messagingSenderId: "295856532208"
+  };
+
+  firebase.initializeApp(config);
+
+  
